8ea45d1b5c72bab681837401af47fe3bebd629bd
Sanket Kolage <60744054+SanketKolage@users.noreply.github.com>
2024-06-28 16:36:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.scss";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./Components/Home/Home.jsx";
-import Header from "./Components/Header/Header.jsx";
+import Home from "./components/Home/Home.jsx";
+import Header from "./components/Header/Header.jsx";
 
 function App() {
   return (
